f34b59112a075c61b30719aff68dd56a6b2c5e32
Ariel Abreu <facekapow@outlook.com>
2020-06-08 22:03:25 -0400

    Fix OS log headers to allow trace_base.h to be included in libtrace

--- libkern/os/trace.h
+++ libkern/os/trace.h
@@ -24,6 +24,9 @@
 #ifndef __OS_TRACE_H__
 #define __OS_TRACE_H__
 
+#ifdef DARLING
+#include <os/trace_base.h>
+#endif
 #include <Availability.h>
 #include <os/base.h>
 #include <sys/types.h>
@@ -42,7 +45,9 @@ typedef void *xpc_object_t;
 
 __BEGIN_DECLS
 
+#ifndef DARLING
 extern void *__dso_handle;
+#endif
 
 OS_ALWAYS_INLINE
 static inline void
