cc94a8f26d6621a4765cabea72607fa3cf378ba5
Tiberiu Chibici <chibicitiberiu@gmail.com>
2021-05-01 15:08:27 -0700

    Fix darling_is_task_64bit call

--- osfmk/duct/duct_kern_thread_act.c
+++ osfmk/duct/duct_kern_thread_act.c
@@ -624,7 +624,7 @@ thread_set_state(
 		{
 			if (state_count < x86_FLOAT_STATE64_COUNT)
 				return KERN_INVALID_ARGUMENT;
-			if (check_64bit_mode(current_pt_regs()))
+			if (!darling_is_task_64bit()))
 				return KERN_INVALID_ARGUMENT;
 
 			const x86_float_state64_t* s = (x86_float_state64_t*) state;
