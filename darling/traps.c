6cde8f0659ca679b5b88c99c61164ffb7445cf03
Ariel Abreu <facekapow@outlook.com>
2021-05-28 16:17:11 -0400

    Fix darlinghq/darling#967
    I'm not sure why this issue was manifesting itself only when compiled without `-O1`, but basically, libdispatch requests MACH_RCV_SYNC_WAIT which requires a valid notification port and we weren't copying it in.
    
    This really shouldn't have ever worked. Weird.

--- darling/traps.c
+++ darling/traps.c
@@ -810,7 +810,7 @@ int mach_msg_overwrite_entry(task_t task, struct mach_msg_overwrite_args* in_arg
 	out.rcv_size = args.recv_size;
 	out.rcv_name = args.rcv_name;
 	out.timeout = args.timeout;
-	// out.notify = args.notify;
+	out.override = args.notify;
 	out.rcv_msg = (user_addr_t) args.rcv_msg;
 
 	return XNU_CONTINUATION_ENABLED(mach_msg_overwrite_trap(&out));
